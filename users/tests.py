63d2ebb1e537ccfd4259beef16017b6fd2964034
Rome Salarda <romxsalarda45@gmail.com>
2025-07-19 23:22:46 +0100

    update user/test.py

--- users/tests.py
+++ users/tests.py
@@ -5,4 +5,4 @@ class SimpleTest(TestCase):
     def test_homepage_status_code(self):
         # Assuming you have a home view at '/'
         response = self.client.get('/redoc')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 301)
