5cd3d0ad226174b58db3a89598221fcf1cf639d3
Rome Salarda <romxsalarda45@gmail.com>
2025-07-19 23:26:21 +0100

    updated instance

--- awstest/settings.py
+++ awstest/settings.py
@@ -175,11 +175,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Static and Media URLs
 if not DEBUG:
-    #AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    #AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    # NOTE: no need to define secret key if using IAM roles
-    # AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    # AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = get_secret("/prod/django/workoutapi/AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = get_secret("/prod/django/workoutapi/AWS_S3_REGION_NAME")
 
