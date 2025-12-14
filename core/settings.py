a73d202e5838b0e622dbd5ae414b9916b88983f5
Rome Salarda <romxsalarda45@gmail.com>
2025-12-14 20:21:49 +0000

    added vercel frontend endpoint

--- core/settings.py
+++ core/settings.py
@@ -315,10 +315,12 @@ else:
     CSRF_TRUSTED_ORIGINS = [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://cems-nine.vercel.app/'
     ]
     CORS_ALLOWED_ORIGINS = [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://cems-nine.vercel.app/'
     ]
     CORS_ALLOW_CREDENTIALS = True  # Required for HTTPOnly cookies
 
